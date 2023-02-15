a92da988d2d5e49c613e7b136852c8d9fa208a94
anushka-ivoyant <74005559+kleiry20@users.noreply.github.com>
2023-02-15 17:18:51 +0530

    created a function in actions.js

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,11 +9,11 @@ function Home() {
         <h3>Home Component</h3>
         <div className="cart-wrapper">
           <div className="img-wrapper item">
-            <img></img>
+            <img alt="image here"></img>
           </div>
           <div className="text-wrapper item">
             <span>Item name</span>
-            <span>Price of the item: $</span>
+            <span>Price of the item: $100</span>
           </div>
           <div className="btn-wrapper item">
             <button>Add Item</button>
